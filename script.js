20d0a42b48a4eac13b174ce986a2ea0b67886420
bhupendra verma <83696409+bunny91-max@users.noreply.github.com>
2023-07-23 04:51:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+gsap.from("#img1",{
+    opacity:0,
+    duration:2,
+    delay:0.5,
+    y:70
+
+})
+gsap.from("#img2",{
+    opacity:0,
+    duration:2,
+    delay:0.5,
+    x:70
+
+})
+gsap.from("#img3",{
+    opacity:0,
+    duration:2,
+    delay:0.5,
+    y:-70
+
+})
+gsap.from("#main h1",{
+    opacity:0,
+    duration:2,
+    delay:0.5,
+    
+
+})
